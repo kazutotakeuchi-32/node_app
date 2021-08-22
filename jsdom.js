fe93f79e54d1614d8d43b828bc4bd3311cb8f022
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-08-22 15:36:57 +0900

    github localhostで表示

---
+++ jsdom.js
@@ -0,0 +1,18 @@
+const request = require('request')
+const { JSDOM } = require('jsdom')
+
+request('https://taketon-blog.com/kazugramming', (e, response, body) => {
+  if (e) {
+    console.error(e)
+  }
+  try {
+    const dom = new JSDOM(body)
+    // console.log(dom.window.document.body.innerHTML);
+    const html = dom.window.document.querySelectorAll("h2")
+    for (let i = 0; i < html.length; i++) {
+      console.log(html[i].textContent.trim());
+    }
+  } catch (e) {
+    console.error(e)
+  }
+})
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const http = require("http")
 const fs = require("fs")
+const request = require('request')
+const { JSDOM } = require('jsdom')
 
-
-const server = http.createServer(async (req,res)=>{
-
+const server = http.createServer(async (req,re)=>{
   const fetch = require("node-fetch")
   const json = await fetch('https://api.github.com/users/kazutotakeuchi-32')
 	.then(res => res.json())
-  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.write(`<a href=${json.html_url}>Hello World</a>`);
-  const a= document.querySelector("a")
+   await request(json.html_url,(e,res,body)=>{
+    re.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    re.write(body);
+    re.end()
+    const dom = new JSDOM(body)
+    console.log(dom.window.document.querySelector("h1").textContent.trim());
+  })
+  // re.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  // re.write(`<a href=${json.html_url}>Hello World</a>`);
+  // const a= document.querySelector("a")
   // fs.readFile('index.html','utf8',(error,data)=>{
   //   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  //   res.write(data)
-  //   res.end();
+  //   res.clwrite(data)
+    // res.end();
   // })
   // res.end(JSON.stringify({ 'foo': 'bar' }));
 })
