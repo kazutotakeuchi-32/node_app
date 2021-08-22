3e2b554ebb20ec5905c7b266f1af13c55b53dead
kazutotakeuchi32 <kazutotakeuchi32@gmail.com>
2021-08-22 11:54:47 +0900

    Webサーバ構築 成功

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
-const fetch = require("node-fetch")
-fetch('https://api.github.com/users/github')
-	.then(res => res.json())
-	.then(json => console.log(json));
+const http = require("http")
+// const fetch = require("node-fetch")
+// fetch('https://api.github.com/users/github')
+// 	.then(res => res.json())
+// 	.then(json => console.log(json));
+
+const server = http.createServer((req,res)=>{
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.write('<h1>Hello World</h1>');
+  res.end();
+})
+
+const port = 4000
+server.listen(port)
+console.log(port);
